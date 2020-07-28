ced90d5cf8ad8599a58888744f830cdbfe195fa3
chistopat <chistopat@gmail.com>
2020-07-28 13:12:50 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,31 +1,7 @@
 #include <iostream>
-#include "vector"
 
 using namespace std;
 
 int main() {
-    int64_t n;
-    cin >> n;
-    if (n == 0) {
-        return 0;
-    }
-    vector<int64_t> temperature(n);
-    int64_t sum = 0;
-    for(auto& item : temperature) {
-        cin >> item;
-        sum += item;
-    }
-    int64_t tempLen = temperature.size();
-    int64_t avg = sum / tempLen;
-    vector<int64_t> upperAverageIndexes;
-    for (int64_t i = 0; i<tempLen; ++i) {
-        if(temperature[i]>avg) {
-            upperAverageIndexes.push_back(i);
-        }
-    }
-    cout << upperAverageIndexes.size() << endl;
-    for (auto& i : upperAverageIndexes) {
-        cout << i << ' ';
-    }
-    return 0;
+   return 0;
 }
