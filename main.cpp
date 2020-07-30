f76d9ed5f96b1267a1d7c69d394a8f0fb26233ff
chistopat <chistopat@gmail.com>
2020-07-31 00:42:47 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include "map"
+#include "string"
 
 using namespace std;
 
+template <typename TKey, typename TValue>
+TValue& GetRefStrict(map<TKey, TValue>& m, const TKey& key);
+
+template <typename TKey, typename TValue>
+TValue& GetRefStrict(map<TKey, TValue>& m, const TKey& key) {
+    if(m.count(key) == 0) {
+        throw runtime_error("Key not found");
+    }
+    return m[key];
+}
+
 int main() {
+    map<int, string> m = {{0, "value"}};
+    string& item = GetRefStrict(m, 0);
+    item = "newvalue";
+    cout << m[0] << endl;
     return 0;
 }
