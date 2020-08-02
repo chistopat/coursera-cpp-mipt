7ec97b3919a5c4613fd39a52d90e080740202b26
chistopat <chistopat@gmail.com>
2020-08-02 14:29:08 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include "algorithm"
+#include "vector"
 
-#include "tests.h"
+void PrintVectorPart(const std::vector<int> &numbers){
+    if (numbers.empty()) {
+        return;
+    }
+    auto firstNegative = std::find_if(numbers.begin(), numbers.end(), [](int x){return x<0;});
+    for (auto it=--firstNegative; it>=numbers.begin(); --it){
+        std::cout << *it << ' ';
+    }
+}
 
 int main() {
-  TestAll();
-  return 0;
+    PrintVectorPart({6, 1, 8, -5, 4});
+    std::cout << std::endl;
+    PrintVectorPart({-6, 1, 8, -5, 4});  // ничего не выведется
+    std::cout << std::endl;
+    PrintVectorPart({6, 1, 8, 5, 4});
+    std::cout << std::endl;
+    PrintVectorPart({});
+    std::cout << std::endl;
+
+    return 0;
 }
--- tests.cpp
+++ tests.cpp
@@ -2,5 +2,5 @@
 
 void TestAll() {
   TestRunner tr;
-  tr.RunTest([]() { std::cout << "OMFG" << std::endl; }, "lambda");
+  tr.RunTest([]() { }, "lambda");
 }
