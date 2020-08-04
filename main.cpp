cd9326eb2fcafad95f13e7ab5926b44ec93720ec
chistopat <chistopat@gmail.com>
2020-08-04 14:28:19 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
 #include "test_runner.h"
 
-int main() {
 
-  return 0;
+template <typename T>
+void RemoveDuplicates(std::vector<T>& elements);
+
+template <typename DataType>
+void RemoveDuplicates(std::vector<DataType>& elements) {
+    sort(begin(elements), end(elements));
+    elements.erase(
+            unique(begin(elements), end(elements)),
+            elements.end());
 }
+
+int main() {
+    std::vector<int> v1 = {6, 4, 7, 6, 4, 4, 0, 1};
+    RemoveDuplicates(v1);
+    for (int x : v1) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    std::vector<std::string> v2 = {"C", "C++", "C++", "C", "C++"};
+    RemoveDuplicates(v2);
+    for (const std::string& s : v2) {
+        std::cout << s << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
+/*
+6 4 7 0 1
+C++ C
+*/
