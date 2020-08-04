1f076b1a1ca825e6c562d5ebfc8b6fc65800a43b
chistopat <chistopat@gmail.com>
2020-08-04 15:06:28 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include "test_runner.h"
 
-int main() {
+void PrintPermutaion(int number) {
+    std::vector<int> digits(number);
+    for (int i=1; i<=number; i++) {
+        digits[i-1]=i;
+    }
+    std::vector<std::vector<int>> results;
+    bool next = true;
+    while(next){
+        results.push_back(digits);
+        next = std::next_permutation(digits.begin(), digits.end());
+    }
+    std::sort(results.rbegin(), results.rend());
+    for(auto& r : results) {
+        for (auto &d : r) {
+            std::cout << d << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
 
-  return 0;
+int main() {
+    int n = 0;
+    std::cin >> n;
+    PrintPermutaion(n);
+    return 0;
 }
