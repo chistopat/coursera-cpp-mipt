7d6d4af4691d60a4a022b1586e7f87c5f001bf13
chistopat <chistopat@gmail.com>
2020-08-24 11:29:21 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include "test_runner.h"
+#include "string"
+#include "set"
+#include "vector"
+
+using namespace std;
+
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+    vector<T> result;
+    auto greaterIt = elements.upper_bound(border);
+    for (auto it=greaterIt; it!=elements.end();) {
+        result.push_back(*it++);
+    }
+    return result;
+};
 
 int main() {
+    for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {
+        cout << x << " ";
+    }
+    cout << endl;
 
-  return 0;
+    string to_find = "Python";
+    cout << FindGreaterElements(set<string>{"C", "C++"}, to_find).size() << endl;
+    return 0;
 }
