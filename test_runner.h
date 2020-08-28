bd1ad6301ae0f4323b97f41ca20c58fdf05c777f
chistopat <chistopat@gmail.com>
2020-08-28 12:47:57 +0300

    commit message

--- test_runner.h
+++ test_runner.h
@@ -70,18 +70,31 @@ void AssertEqual(const std::vector<T> &t, const std::vector<U> &u,
 
 class TestRunner {
 public:
-  template <class TestFunc>
-  void RunTest(TestFunc func, const std::string &test_name) {
-    try {
-      func();
-      std::cerr << test_name << " OK" << std::endl;
-    } catch (std::runtime_error &e) {
-      ++fail_count;
-      std::cerr << test_name << " fail: " << e.what() << std::endl;
+    template <class TestFunc>
+    void RunTest(TestFunc func, const std::string& test_name) {
+        try {
+            func();
+            std::cerr << test_name << " OK" << std::endl;
+        } catch (std::exception& e) {
+            ++fail_count;
+            std::cerr << test_name << " fail: " << e.what() << std::endl;
+        } catch (...) {
+            ++fail_count;
+            std::cerr << "Unknown exception caught" << std::endl;
+        }
     }
-  }
-  ~TestRunner();
+
+    ~TestRunner() {
+        if (fail_count > 0) {
+            std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
+            exit(1);
+        }
+    }
 
 private:
-  int fail_count = 0;
+    int fail_count = 0;
 };
+
+inline void Assert(bool b, const std::string &hint) {
+    AssertEqual(b, true, hint);
+}
