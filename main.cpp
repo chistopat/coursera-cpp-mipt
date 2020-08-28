6974dd406cc1391ca8a05cb34e4ac5cbc50567f1
chistopat <chistopat@gmail.com>
2020-08-28 18:18:44 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
 #include "test_runner.h"
 
+#include <ostream>
+using namespace std;
+
+#define PRINT_VALUES(out, x, y) out << (x) << endl << (y) << endl \
+
 int main() {
     TestRunner tr;
+    tr.RunTest([] {
+        ostringstream output;
+        PRINT_VALUES(output, 5, "red belt");
+        ASSERT_EQUAL(output.str(), "5\nred belt\n");
+    }, "PRINT_VALUES usage example");
+    tr.RunTest([] {
+        ostringstream output;
+        int x = 1;
+        PRINT_VALUES(output, ++x, "red belt");
+        ASSERT_EQUAL(output.str(), "2\nred belt\n");
+    }, "x++");
+    tr.RunTest([] {
+        ostringstream output;
+        int x = 1;
+        PRINT_VALUES(output, ++x, "red belt");
+        ASSERT_EQUAL(output.str(), "2\nred belt\n");
+    }, "x++");
+    tr.RunTest([] {
+        ostringstream output;
+        int x = 1;
+        if (true)
+            PRINT_VALUES(output, ++x, "red belt");
+        else
+            PRINT_VALUES(output, ++x, "red belt");
+
+        ASSERT_EQUAL(output.str(), "\nred belt\n");
+    }, "x++");
+
 }
