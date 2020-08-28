dbba0eb7601691f16742bd7e953a026343ee5e02
chistopat <chistopat@gmail.com>
2020-08-28 14:52:47 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -2,14 +2,25 @@
 #include "test_runner.h"
 #include "utility"
 
-#include <algorithm>
-#include <numeric>
 using namespace std;
 
-#define SORT_BY(field)  [](const AirlineTicket& lhs, const AirlineTicket& rhs){return lhs.field < rhs.field;}
+#define UPDATE_FIELD(ticket, field, values) { \
+    auto it = values.find(#field); \
+    if (it != values.end()) { \
+        istringstream is(it->second); \
+        is >> ticket.field; \
+    }\
+} // Реализуйте этот макрос, а также необходимые операторы для классов Date и Time
 
-bool operator<(const Date& lhs, const Date& rhs) {
-    return std::tie(lhs.year, lhs.month, lhs.day) < std::tie(rhs.year, rhs.month, rhs.day);
+istream& operator>>(istream& is, Date& date) {
+    char dlm;
+    is >> date.year >> dlm >> date.month >> dlm >> date.day;
+    return is;
+}
+ostream& operator<<(ostream& os, const Date& date);
+ostream& operator<<(ostream& os, const Date& date) {
+    os << date.year << '-' << date.month << '-' << date.day;
+    return os;
 }
 
 bool operator==(const Date& lhs, const Date& rhs) {
@@ -20,52 +31,86 @@ bool operator!=(const Date& lhs, const Date& rhs) {
     return std::tie(lhs.year, lhs.month, lhs.day) != std::tie(rhs.year, rhs.month, rhs.day);
 }
 
-ostream& operator<<(ostream& os, const Date& date) {
-    os <<  date.year << '-' << date.month << '-' << date.day;
-    return os;
+bool operator<(const Date& lhs, const Date& rhs) {
+    return std::tie(lhs.year, lhs.month, lhs.day) < std::tie(rhs.year, rhs.month, rhs.day);
 }
 
-bool operator<(const Time& lhs, const Time& rhs) {
-    return std::tie(lhs.hours, lhs.minutes) < std::tie(rhs.hours, rhs.minutes);
+bool operator>(const Date& lhs, const Date& rhs) {
+    return std::tie(lhs.year, lhs.month, lhs.day) > std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+istream& operator>>(istream& is, Time& time) {
+    char dlm;
+    is >> time.hours >> dlm >> time.minutes;
+    return is;
+}
+
+ostream& operator<<(ostream& os, const Time& time) {
+    os << time.hours << ':' << time.minutes;
+    return os;
 }
 
 bool operator==(const Time& lhs, const Time& rhs) {
     return std::tie(lhs.hours, lhs.minutes) == std::tie(rhs.hours, rhs.minutes);
 }
 
-bool operator!=(const Time& lhs, const Time& rhs) {
-    return std::tie(lhs.hours, lhs.minutes) != std::tie(rhs.hours, rhs.minutes);
-}
+void TestUpdate() {
+    AirlineTicket t;
+    t.price = 0;
 
-ostream& operator<<(ostream& os, const Time& time) {
-    os <<  time.hours << ':' << time.minutes;
-    return os;
-}
-
-
-void TestSortBy() {
-    vector<AirlineTicket> tixs = {
-            {"VKO", "AER", "Utair",     {2018, 2, 28}, {17, 40}, {2018, 2, 28}, {20,  0}, 1200},
-            {"AER", "VKO", "Utair",     {2018, 3,  5}, {14, 15}, {2018, 3,  5}, {16, 30}, 1700},
-            {"AER", "SVO", "Aeroflot",  {2018, 3,  5}, {18, 30}, {2018, 3,  5}, {20, 30}, 2300},
-            {"PMI", "DME", "Iberia",    {2018, 2,  8}, {23, 00}, {2018, 2,  9}, { 3, 30}, 9000},
-            {"CDG", "SVO", "AirFrance", {2018, 3,  1}, {13, 00}, {2018, 3,  1}, {17, 30}, 8000},
+    const map<string, string> updates1 = {
+            {"departure_date", "2018-2-28"},
+            {"departure_time", "17:40"},
     };
+    UPDATE_FIELD(t, departure_date, updates1);
+    UPDATE_FIELD(t, departure_time, updates1);
+    UPDATE_FIELD(t, price, updates1);
 
-    sort(begin(tixs), end(tixs), SORT_BY(price));
-    ASSERT_EQUAL(tixs.front().price, 1200);
-    ASSERT_EQUAL(tixs.back().price, 9000);
+    ASSERT_EQUAL(t.departure_date, (Date{2018, 2, 28}));
+    ASSERT_EQUAL(t.departure_time, (Time{17, 40}));
+    ASSERT_EQUAL(t.price, 0);
 
-    sort(begin(tixs), end(tixs), SORT_BY(from));
-    ASSERT_EQUAL(tixs.front().from, "AER");
-    ASSERT_EQUAL(tixs.back().from, "VKO");
+    const map<string, string> updates2 = {
+            {"price", "12550"},
+            {"arrival_time", "20:33"},
+    };
+    UPDATE_FIELD(t, departure_date, updates2);
+    UPDATE_FIELD(t, departure_time, updates2);
+    UPDATE_FIELD(t, arrival_time, updates2);
+    UPDATE_FIELD(t, price, updates2);
 
-    sort(begin(tixs), end(tixs), SORT_BY(arrival_date));
-    ASSERT_EQUAL(tixs.front().arrival_date, (Date{2018, 2, 9}));
-    ASSERT_EQUAL(tixs.back().arrival_date, (Date{2018, 3, 5}));
+    // updates2 не содержит ключей "departure_date" и "departure_time", поэтому
+    // значения этих полей не должны измениться
+    ASSERT_EQUAL(t.departure_date, (Date{2018, 2, 28}));
+    ASSERT_EQUAL(t.departure_time, (Time{17, 40}));
+    ASSERT_EQUAL(t.price, 12550);
+    ASSERT_EQUAL(t.arrival_time, (Time{20, 33}));
 }
 
 int main() {
     TestRunner tr;
-    RUN_TEST(tr, TestSortBy);
+    RUN_TEST(tr, TestUpdate);
 }
+
+
+//void UpdateTicket(AirlineTicket& ticket, const map<string, string>& updates) {
+//    map<string, string>::const_iterator it;
+//
+//    it = updates.find("to");
+//    if (it != updates.end()) {
+//        ticket.to = it->second;
+//    }
+//
+//    it = updates.find("from");
+//    if (it != updates.end()) {
+//        ticket.from = it->second;
+//    }
+//
+//    it = updates.find("price");
+//    if (it != updates.end()) {
+//        istringstream is(it->second);
+//        is >> ticket.price;
+//    }
+//
+//    ...
+//}
