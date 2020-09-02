f83465e326f1e4c4dafb295ff0dfffa07b0f5c2a
chistopat <chistopat@gmail.com>
2020-09-02 22:44:12 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include "test_runner.h"
 #include "profile.h"
 
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Learner {
+private:
+  set<string> dict;
+
+public:
+  int Learn(const vector<string>& words) {
+    int newWords = 0;
+    for (const auto& word : words) {
+      if (dict.count(word) == 0) {
+        ++newWords;
+        dict.insert(word);
+      }
+    }
+    return newWords;
+  }
+
+  vector<string> KnownWords() {
+    return {dict.begin(), dict.end()};
+  }
+};
+
 int main() {
-    TestRunner tr;
+  Learner learner;
+  string line;
+  while (getline(cin, line)) {
+    vector<string> words;
+    stringstream ss(line);
+    string word;
+    while (ss >> word) {
+      words.push_back(word);
+    }
+    cout << learner.Learn(words) << "\n";
+  }
+  cout << "=== known words ===\n";
+  for (auto word : learner.KnownWords()) {
+    cout << word << "\n";
+  }
 }
