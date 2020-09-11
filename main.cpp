3032a7e1b734545291b540590bc32f2aa45d3b92
chistopat <chistopat@gmail.com>
2020-09-11 16:54:28 +0300

    6/17

--- main.cpp
+++ main.cpp
@@ -1,175 +1,143 @@
-#include <iomanip>
-#include <iostream>
-#include <vector>
-#include "set"
+#include "iostream"
+#include "string"
 #include "map"
-#include "numeric"
-#include "profile.h"
-#include "test_runner.h"
+#include "cinttypes"
+#include "deque"
 #include "algorithm"
-#include "numeric"
+#include "set"
+#include "test_runner.h"
+
+typedef std::string THotelName;
+typedef int TClientId;
 
 using namespace std;
 
-typedef int TPageNumber, TUserId;
-
-struct Reader {
-  TUserId userId;
-  TPageNumber page_count;
+struct Accomodation {
+  int64_t time;
+  int user_id;
+  int rooms_count;
 };
 
-bool operator<(const Reader& lhs, const Reader& rhs) {
-  if (lhs.page_count == rhs.page_count) {
-    return lhs.userId < rhs.userId;
-  }
-  return lhs.page_count < rhs.page_count;
-}
+struct Hotel {
+  deque<Accomodation> accommodations;
+  int booked_rooms_count;
+  set<TClientId> clients;
+};
 
-//class ReadingManager {
-//public:
-//  ReadingManager() = default;
-//  void Read(int user_id, int page_count) {
-//    if (_users.count(user_id) == 1) {
-//      _pages_to_readers[_users[user_id]].erase(user_id);
-//    }
-//    _pages_to_readers[page_count].insert(user_id);
-//    _users[user_id] = page_count;
-//  }
-//  double Cheer(int user_id) const {
-//    if (_users.count(user_id) == 0) {
-//      return 0;
-//    }
-//    int total = _users.size();
-//    if (total == 1) {
-//      return 1;
-//    }
-//    int count = 0;
-//    const int& current_user_page = _users.at(user_id);
-//
-//    for(int i = 0; i < current_user_page; ++i) {
-//      if (_pages_to_readers.count(i) == 1) {
-//        count += _pages_to_readers.at(i).size();
-//      }
-//    }
-//    return  (count * 1.0) / --total;
-//  }
-//private:
-//  map<TPageNumber, set<TUserId>> _pages_to_readers;
-//  map<TUserId, TPageNumber> _users;
-//};
-
-class ReadingManager {
+class BookingManager {
 public:
-  ReadingManager() : rating(1000, 0) {}
-  void Read(int user_id, int page_count) {
-    if(users.count(user_id) == 1) {
-      rating[users[user_id]]--;
-    }
-    users[user_id] = page_count;
-    rating[page_count]++;
+  void Book(string hotel_name, int64_t time, int user_id, int rooms_count) {
+    Accomodation accomodation({time, user_id, rooms_count});
+    _CheckOut(time);
+    _CheckIn(accomodation, hotel_name);
+
   }
-  double Cheer(int user_id) const {
-    if (users.count(user_id) == 0) {
+  const int Rooms(string hotel_name) const {
+    int count = 0;
+    if (_database.count(hotel_name) == 0) {
       return 0;
     }
-    int total = users.size();
-    if (total == 1) {
-      return 1;
-    }
-    const auto& current_page = users.at(user_id);
-    int users_count = 0;
-    for (int i = 0; i < current_page; ++i) {
-      users_count += rating[i];
-    }
-    return users_count * 1.0 / --total;
+    return _database.at(hotel_name).booked_rooms_count;
   }
+  const int Clients(string hotel_name) const {
+    set<int> users;
+    if (_database.count(hotel_name) == 0) {
+      return 0;
+    }
+    return _database.at(hotel_name).clients.size();
+  }
+
 private:
-  vector<int> rating;
-  std::map<TUserId , TPageNumber> users;
+  map<THotelName, Hotel> _database;
+  const int dayDurationSeconds = 86400;
+
+  void _CheckIn(Accomodation accomodation, const string& hotel_name) {
+    _database[hotel_name].accommodations.emplace_back(accomodation);
+    _database[hotel_name].clients.insert(accomodation.user_id);
+    _database[hotel_name].booked_rooms_count += accomodation.rooms_count;
+  }
+
+  void _CheckOut(int64_t time) {
+    auto Predicate = [&](Accomodation entry){
+      return (time - entry.time) < dayDurationSeconds;
+    };
+    //iterate by hotels
+    for (auto& [hotel_name, hotel] : _database) {
+      //move all expired accomodations to tail
+      auto firstExpiredIt = std::find_if(begin(hotel.accommodations),
+                                      end(hotel.accommodations),
+                                      Predicate);
+      for (auto it = hotel.accommodations.begin(); it != firstExpiredIt; it++) {
+        hotel.booked_rooms_count -= it->rooms_count;
+        hotel.clients.erase(it->user_id);
+      }
+      hotel.accommodations.erase( begin(hotel.accommodations), firstExpiredIt);
+    }
+  }
+
 };
 
-
-void TestRead() {
-  LogDuration ld("TestRead");
-  int MAX = 1'000'000;
-  vector<int> users(MAX);
-  vector<int> pages(MAX);
-
-  std::iota(users.begin(), users.end(), 1);
-  for(int i = 0; i < 1000; i++) {
-    auto f = users.begin();
-    iota(f, f+1000, 1);
-  }
-  ReadingManager rm;
-  for(int i = 0; i < MAX; i++) {
-    rm.Read(users[i], pages[i]);
-  }
+void Test() {
+  BookingManager bm;
+  ASSERT_EQUAL(bm.Clients("Marriott"), 0);
+  ASSERT_EQUAL(bm.Rooms("Marriott"), 0);
+  bm.Book("FourSeasons", 10, 1, 2);
+  bm.Book("Marriott", 10, 2, 1);
+  bm.Book("FourSeasons", 86409, 2, 1);
+  ASSERT_EQUAL(bm.Rooms("FourSeasons"), 3);
+  ASSERT_EQUAL(bm.Clients("FourSeasons"), 2);
+  ASSERT_EQUAL(bm.Clients("Marriott"), 1);
+  bm.Book("Marriott", 86410, 2, 10);
+  ASSERT_EQUAL(bm.Rooms("FourSeasons"), 1);
+  ASSERT_EQUAL(bm.Rooms("Marriott"), 10);
 }
 
-void TestCheer() {
-  LogDuration ld("TestCheer");
-  ReadingManager rm;
-  ASSERT_EQUAL(rm.Cheer(5), 0);
-  rm.Read(1, 10);
-  ASSERT_EQUAL(rm.Cheer(1), 1);
-  rm.Read(2, 5);
-  rm.Read(3, 7);
-  ASSERT_EQUAL(rm.Cheer(2), 0);
-  ASSERT_EQUAL(rm.Cheer(3), 0.5);
-  rm.Read(3, 10);
-  ASSERT_EQUAL(rm.Cheer(3), 0.5);
+void TestAll() {
+  TestRunner tr;
+  RUN_TEST(tr, Test);
 }
 
-void TestSamePages() {
-  ReadingManager rm;
-  rm.Read(1, 1);
-  rm.Read(2, 2);
-  rm.Read(3, 2);
-  rm.Read(4, 2);
-  rm.Read(5, 2);
-  ASSERT_EQUAL(rm.Cheer(2), (1.0/4));
-  rm.Read(1, 2);
-  ASSERT_EQUAL(rm.Cheer(2), 0);
-  ASSERT_EQUAL(rm.Cheer(1), 0);
-}
+enum class CommandTypes {
+  Book,
+  Clients,
+  Rooms
+};
 
-void TestOneCheer() {
-  ReadingManager rm;
-  rm.Read(125, 1);
-  ASSERT_EQUAL(rm.Cheer(999), 0);
-}
+map<string, CommandTypes> commands_map{
+    {"BOOK", CommandTypes::Book},
+    {"CLIENTS", CommandTypes::Clients},
+    {"ROOMS", CommandTypes::Rooms}
+};
 
 int main() {
-  // Для ускорения чтения данных отключается синхронизация
-  // cin и cout с stdio,
-  // а также выполняется отвязка cin от cout
-  TestRunner tr;
-  RUN_TEST(tr, TestCheer);
-  RUN_TEST(tr, TestSamePages);
-  RUN_TEST(tr, TestOneCheer);
-
+  TestAll();
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-
-  ReadingManager manager;
-
-  int query_count;
-  cin >> query_count;
-
-  for (int query_id = 0; query_id < query_count; ++query_id) {
-    string query_type;
-    cin >> query_type;
-    int user_id;
-    cin >> user_id;
-
-    if (query_type == "READ") {
-      int page_count;
-      cin >> page_count;
-      manager.Read(user_id, page_count);
-    } else if (query_type == "CHEER") {
-      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+  int n = 0;
+  std::cin >> n;
+  BookingManager bookingManager;
+  while (n--) {
+    string command;
+    std::cin >> command;
+    string hotel_name;
+    switch (commands_map[command]) {
+      case CommandTypes::Book: {
+        int64_t time = 0;
+        int client_id = 0;
+        int room_count = 0;
+        cin >> time >> hotel_name >> client_id >> room_count;
+        bookingManager.Book(hotel_name, time, client_id, room_count);
+        break;
+      }
+      case CommandTypes::Rooms:
+        cin >> hotel_name;
+        cout << bookingManager.Rooms(hotel_name) << '\n';
+        break;
+      case CommandTypes::Clients:
+        cin >> hotel_name;
+        cout << bookingManager.Clients(hotel_name) << '\n';
+        break;
     }
-  }
 
-  return 0;
+  }
 }
