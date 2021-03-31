af565ed4817130cdd17202a95915afd6c5f300e8
chistopat <chistopat@gmail.com>
2021-03-31 19:47:19 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,11 +1,44 @@
-#include "test_runner.h"
 #include "profile.h"
+#include "test_runner.h"
 
+#include <iostream>
+#include <list>
+#include <map>
 
-using namespace std;
+class Football {
+public:
+  void Add(int current, int previous) {
+    if (!_sportsmens_map.count(previous)) {
+      _sportsmens_map[current] =
+          _sportsmens.insert(_sportsmens.end(), current);
+    } else {
+      _sportsmens_map[current] =
+          _sportsmens.insert(_sportsmens_map[previous], current);
+    }
+  }
+  void Print() const {
+    for (const auto i : _sportsmens) {
+      std::cout << i << std::endl;
+    }
+  }
 
+private:
+  using Position = std::list<int>::iterator;
 
+private:
+  std::list<int> _sportsmens;
+  std::map<int, Position> _sportsmens_map;
+};
 
 int main() {
+  size_t n = 0;
+  std::cin >> n;
+  Football football;
+  for (size_t i = 0; i < n; ++i) {
+    int current = 0, previous = 0;
+    std::cin >> current >> previous;
+    football.Add(current, previous);
+  }
+  football.Print();
   return 0;
 }
