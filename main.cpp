b8b34f22db0ee104f87249e538bda005780aedb1
chistopat <chistopat@gmail.com>
2021-04-21 17:16:17 +0300

    merge3

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@
 #include <iostream>
 #include <iterator>
 #include <functional>
+#include <memory>
 
 using namespace std;
 
-
-std::ostream& operator<<(std::ostream& os, std::vector<int> v) {
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& v) {
     os << "[";
     bool first = true;
     for (const auto &i : v) {
@@ -27,6 +27,21 @@ std::ostream& operator<<(std::ostream& os, std::vector<int> v) {
     os << "]";
     return os;
 }
+struct NoncopyableInt {
+  int value;
+
+  NoncopyableInt(int value) : value(value) {}
+
+  NoncopyableInt(const NoncopyableInt&) = delete;
+  NoncopyableInt& operator=(const NoncopyableInt&) = delete;
+
+  NoncopyableInt(NoncopyableInt&&) = default;
+  NoncopyableInt& operator=(NoncopyableInt&&) = default;
+};
+
+bool operator<(const NoncopyableInt& left, const NoncopyableInt& rigth) {
+    return left.value < rigth.value;
+}
 
 template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end) {
@@ -34,29 +49,30 @@ void MergeSort(RandomIt range_begin, RandomIt range_end) {
     if(range_end - range_begin < 2) {
         return;
     }
-    vector<typename RandomIt::value_type> elements(range_begin, range_end);
+    vector<typename RandomIt::value_type> elements(make_move_iterator(range_begin), make_move_iterator(range_end));
     auto border = distance(range_begin, range_end) / 3;
     auto begin = elements.begin();
     auto m1 = elements.begin() + border;
     auto m2 = elements.begin() + border + border;
     auto end = elements.end();
-    MergeSort(begin, m1);
-    MergeSort(m1, m2);
-    MergeSort(m2, end);
+    MergeSort(make_move_iterator(begin), make_move_iterator(m1));
+    MergeSort(make_move_iterator(m1), make_move_iterator(m2));
+    MergeSort(make_move_iterator(m2), make_move_iterator(end));
     vector<typename RandomIt::value_type> temp;
-    merge(begin, m1, m1, m2, back_inserter(temp));
-    merge(temp.begin(), temp.end(), m2, end, range_begin);
-
+    merge(make_move_iterator(begin), make_move_iterator(m1), make_move_iterator(m1), make_move_iterator(m2), back_inserter(temp));
+    merge(make_move_iterator(temp.begin()), make_move_iterator(temp.end()), make_move_iterator(m2), make_move_iterator(end), range_begin);
 }
 
-
+void TestIntVector() {
+    vector<NoncopyableInt> numbers;
+    for (auto i : vector<int>{6, 1, 3, 9, 1, 9, 8, 12, 1}) {
+        numbers.emplace_back(i);
+    }
+    MergeSort(begin(numbers), end(numbers));
+    assert(is_sorted(begin(numbers), end(numbers)));
+}
 
 int main() {
-    vector<int> v = {6, 4, 7, 6, 4, 4, 0, 1, 9};
-    MergeSort(begin(v), end(v));
-    for (int x : v) {
-        cout << x << " ";
-    }
-    cout << endl;
+    TestIntVector();
     return 0;
 }
